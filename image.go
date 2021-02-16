566919fc5e24f5ee7a052de71c829cb497a361da
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-02-15 21:18:33 -0800

    Starting to implement image URLs

---
+++ image.go
@@ -0,0 +1,17 @@
+package gog_urls
+
+import "net/url"
+
+const pngExt = ".png"
+
+func Image(srcImage string) (*url.URL, error) {
+	imgUrl, err := url.Parse(srcImage)
+	if err != nil {
+		return nil, err
+	}
+
+	imgUrl.Scheme = HttpsScheme
+	imgUrl.Path += pngExt
+
+	return imgUrl, nil
+}
