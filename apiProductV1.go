685a9fc2187d870c5ce1f25e281db3ab49230885
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-02-26 22:12:05 -0800

    Bug fix: ugh :-) actually add query params to V1, not V2

--- apiProductV1.go
+++ apiProductV1.go
@@ -7,9 +7,15 @@ import (
 )
 
 func ApiProductV1(id string, mt gog_types.Media) *url.URL {
-	return &url.URL{
+	apv1url := &url.URL{
 		Scheme: HttpsScheme,
 		Host:   apiHost,
 		Path:   strings.Replace(apiV1PathTemplate, "{id}", id, 1),
 	}
+
+	q := apv1url.Query()
+	q.Set("expand", strings.Join(expandValues, ","))
+	apv1url.RawQuery = q.Encode()
+
+	return apv1url
 }
--- apiProductV2.go
+++ apiProductV2.go
@@ -3,7 +3,6 @@ package gog_urls
 import (
 	"github.com/arelate/gog_types"
 	"net/url"
-	"strings"
 )
 
 var expandValues = []string{
@@ -16,15 +15,9 @@ var expandValues = []string{
 	"changelog"}
 
 func ApiProductV2(id string, mt gog_types.Media) *url.URL {
-	apv2url := &url.URL{
+	return &url.URL{
 		Scheme: HttpsScheme,
 		Host:   apiHost,
 		Path:   apiV2GamesPath + id,
 	}
-
-	q := apv2url.Query()
-	q.Set("expand", strings.Join(expandValues, ","))
-	apv2url.RawQuery = q.Encode()
-
-	return apv2url
 }
